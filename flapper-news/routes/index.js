6d2932cd94a6f2b319191aac852d7e1609f95fd0
botello <betodelrio@gmail.com>
2015-10-12 14:18:32 +0000

    Wiring posts/comments with backend
    Including adding posts/comments and upvoting

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -84,7 +84,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
   });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', function(req, req, next) {
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
   req.comment.upvote(function (err, comment) {
     if (err) { return next(err); }
 
